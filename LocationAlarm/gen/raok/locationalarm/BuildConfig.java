e477a8ae971f73cf76a8ab18ac87200f8acf8c76
krishnaz <krishnajrao@gmail.com>
2012-07-12 21:56:38 +0100

    Add skeleton code and tests.

---
+++ LocationAlarm/gen/raok/locationalarm/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package raok.locationalarm;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ LocationAlarmTest/gen/raok/locationalarm/test/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package raok.locationalarm.test;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
